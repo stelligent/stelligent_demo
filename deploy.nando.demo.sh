369064554a4d4aa7d9a62903b18047ae4a49e42d
Fernando Pando <nando@stella.hex7.com>
2015-03-26 18:00:23 -0400

    update deploy bash script

--- deploy.nando.demo.sh
+++ deploy.nando.demo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-aws cloudformation update-stack --stack-name nando-demo --template-body  file://cfn/demo.cfn.json
+aws cloudformation create-stack --stack-name nando-demo --template-body  file://cloudformation.json
 while (!$(aws cloudformation describe-stacks --stack-name nando-demo|grep CREATE_COMPLETE)); do sleep 10; done
 jenkins = $(aws cloudformation describe-stacks --stack-name nando-demo | grep jenkins | cut -f4)
 scp -i ~/stelligent/nando_demo.pem ~/stelligent/nando_demo.pem ec2-user@jenkins:
