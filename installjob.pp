9a1d7505695ae824a949319c11dbc8eaa98b7936
Fernando J Pando <fernando.pando@stelligent.com>
2015-03-26 23:11:06 -0400

    pp files

--- installjob.pp
+++ installjob.pp
@@ -1,10 +1,5 @@
 node /.*internal$/ {
    	include jenkins
-	
-	jenkins::plugin {
-  		"git" : ;
-	}
-
 	jenkins::job { 'nando-automation-demo':
   		config => template("/etc/puppet/nando-automation-demo.xml.erb"),
 	}
