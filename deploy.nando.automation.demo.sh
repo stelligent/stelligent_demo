5330a222a0e70568465c00c03fec0e3e5073f347
Fernando J Pando <fernando.pando@stelligent.com>
2015-03-27 15:11:53 -0400

    delete existing keypair

--- deploy.nando.automation.demo.sh
+++ deploy.nando.automation.demo.sh
@@ -4,24 +4,20 @@ echo
 echo "Nando Automation Demo Launch Script"
 echo
 keyName="nando-demo"
-existingKeypair=$(aws ec2 describe-key-pairs | cut -f3) 
-echo
-echo "Ensure $keyName keypair does not exist:"
-echo
-for thisKeypair in $existingKeypair; do
-	echo "testing:... $thisKeypair:$keyName"
-	if [[ $thisKeypair == $keyName ]]; then 
-		echo "deleting keypair:... $thisKeypair:$keyName"
-		aws ec2 delete-key-pair --key-name $keyName
-	fi 
-done
+existingKeypair=$(aws ec2 describe-key-pairs --key-name $keyName) 
+if [ ! -z "$existingKeypair" ]; then 
+	echo
+	echo "Deleting existing $keyName keypair:... $existingKeypair"; 
+	aws ec2 delete-key-pair --key-name $keyName
+	echo
+fi
 echo
 echo "Creating $keyName keypair:"
 echo
-keyValue=$(aws ec2 create-key-pair --key-name $keyName)
-echo $keyValue
+privateKeyValue=$(aws ec2 create-key-pair --key-name $keyName)
+echo $privateKeyValue
 echo
 
-#aws cloudformation create-stack --stack-name nando-demo --template-body  file://cloudformation.json
+aws cloudformation create-stack --stack-name nando-demo --template-body file://cloudformation.json --parameters ParameterKey=privateKey,ParameterValue=$privateKeyValue
 #while (!$(aws cloudformation describe-stacks --stack-name nando-demo|grep CREATE_COMPLETE)); do sleep 10; done
 #echo "\n\nThe Demo has been deployed.\n\n"
