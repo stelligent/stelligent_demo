70d99f2b1ac03372e3eb71bdbc6bb6d3809b2b07
nando p <fernando.pando@stelligent.com>
2015-04-08 17:33:37 -0400

    codedeploy

--- codedeploy.sh
+++ codedeploy.sh
@@ -1,35 +1,12 @@
 #!/bin/bash
 
-asg="nando-demo-20150408161902-NandoDemoWebASG-LWF1YVDXXLZ3"
+asg="nando-demo-20150408165215-NandoDemoWebASG-1FP2BAWBSDD32"
+commitID="b423902310bd4dd46d3704c948815f34b4a0d0e3"
 
 #aws deploy create-application --application-name nando-demo
-
-echo
-echo "delete existing role and policy"
-echo
-aws iam delete-role-policy --role-name NandoDemoCodeDeployRole --policy-name NandoDemoCodeDeployPolicy 2> /dev/null
-aws iam delete-role --role-name NandoDemoCodeDeployRole 2> /dev/null
-sleep 2
-echo
-echo "create codedeploy role and policy"
-echo
 aws iam create-role --role-name NandoDemoCodeDeployRole --assume-role-policy-document file://codedeploy/NandoDemoCodeDeployRole.json
-sleep 3 
 aws iam put-role-policy --role-name NandoDemoCodeDeployRole --policy-name NandoDemoCodeDeployPolicy --policy-document file://codedeploy/NandoDemoCodeDeployPolicy.json
-sleep 5 
-echo
-echo "Create codedeploy deployment-group"
-echo
 roleArn=$(aws iam get-role --role-name NandoDemoCodeDeployRole --query "Role.Arn" --output text)
-aws deploy delete-deployment-group --application-name nando-demo --deployment-group-name nando-demo 2> /dev/null
 aws deploy create-deployment-group --application-name nando-demo --deployment-group-name nando-demo --service-role-arn $roleArn --auto-scaling-group $asg
-sleep 1
-echo
-echo "get codedeploy deployment ID"
-echo
-deployID=$(aws deploy create-deployment --application-name nando-demo  --github-location commitId=5816419ce6eeb3b4dd996297773cfe83f50c1488,repository=stelligent/nando_automation_demo --deployment-group-name nando-demo)
-echo
-echo "get $deployID status"
-echo
+deployID=$(aws deploy create-deployment --application-name nando-demo  --github-location commitId=$commitID,repository=stelligent/nando_automation_demo --deployment-group-name nando-demo)
 aws deploy get-deployment --deployment-id $deployID  --query "deploymentInfo.status" --output text
-echo
