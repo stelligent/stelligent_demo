b423902310bd4dd46d3704c948815f34b4a0d0e3
nando p <fernando.pando@stelligent.com>
2015-04-08 16:58:26 -0400

    codedeploy

--- codedeploy.sh
+++ codedeploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-asg="nando-demo-20150408150144-NandoDemoWebASG-W567NRZGRCHA"
+asg="nando-demo-20150408161902-NandoDemoWebASG-LWF1YVDXXLZ3"
 
 #aws deploy create-application --application-name nando-demo
 
@@ -9,14 +9,14 @@ echo "delete existing role and policy"
 echo
 aws iam delete-role-policy --role-name NandoDemoCodeDeployRole --policy-name NandoDemoCodeDeployPolicy 2> /dev/null
 aws iam delete-role --role-name NandoDemoCodeDeployRole 2> /dev/null
-sleep 1
+sleep 2
 echo
 echo "create codedeploy role and policy"
 echo
 aws iam create-role --role-name NandoDemoCodeDeployRole --assume-role-policy-document file://codedeploy/NandoDemoCodeDeployRole.json
-sleep 2
+sleep 3 
 aws iam put-role-policy --role-name NandoDemoCodeDeployRole --policy-name NandoDemoCodeDeployPolicy --policy-document file://codedeploy/NandoDemoCodeDeployPolicy.json
-sleep 2
+sleep 5 
 echo
 echo "Create codedeploy deployment-group"
 echo
