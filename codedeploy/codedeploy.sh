febb03238e24943a88225bb4fded91d45dfe5f6c
nando p <fernando.pando@stelligent.com>
2015-04-09 00:25:14 -0400

    codedeploy

--- codedeploy/codedeploy.sh
+++ codedeploy/codedeploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 commitID=$(git rev-parse --verify HEAD)
+echo
+echo "using github commit $commitID"
+echo
 deployID=$(aws deploy create-deployment --application-name nando-demo  --github-location commitId=$commitID,repository=stelligent/nando_automation_demo --deployment-group-name nando-demo)
 aws deploy get-deployment --deployment-id $deployID  --query "deploymentInfo.status" --output text
+echo
 
