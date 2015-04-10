15a1e085d025801f9ec01eabd2f8952563832d2c
nando p <fernando.pando@stelligent.com>
2015-04-10 00:52:37 -0400

    paths

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -fv docker/nando-demo.zip
-zip docker/nando-demo.zip docker/Dockerfile docker/application.py docker/requirements.txt
-aws s3 cp docker/nando-demo.zip s3://nando-automation-demo
-aws cloudformation create-stack --stack-name nando-demo-docker --template-body file://docker/elasticbeanstalk.json
+rm -fv nando-demo.zip
+zip nando-demo.zip Dockerfile application.py requirements.txt
+aws s3 cp nando-demo.zip s3://nando-automation-demo
+aws cloudformation create-stack --stack-name nando-demo-docker --template-body file://elasticbeanstalk.json
