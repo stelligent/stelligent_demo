add99ac10c49a1914f9d00ad5063ec04d2e25600
nando p <fernando.pando@stelligent.com>
2015-04-10 09:46:48 -0400

    docker

--- docker/application.py
+++ docker/application.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'nando-automation-demo via docker'
+    return 'nando-automation-demo via docker!!'
 
 if __name__ == '__main__':
     app.run()
--- docker/docker.sh
+++ docker/docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+stackName="nando-demo-docker-$(date +%Y%m%d%H%M%S)"
 rm -fv nando-demo.zip
 zip nando-demo.zip Dockerfile application.py requirements.txt
 aws s3 cp nando-demo.zip s3://nando-automation-demo
-aws cloudformation create-stack --stack-name nando-demo-docker --template-body file://elasticbeanstalk.json
+aws cloudformation create-stack --stack-name $stackName --template-body file://elasticbeanstalk.json
