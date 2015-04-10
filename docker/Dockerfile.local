fc682fcfaaa83f9870e43bc5f7733923d206d136
nando p <fernando.pando@stelligent.com>
2015-04-10 00:42:12 -0400

    docker bootstrap

--- docker/Dockerfile.local
+++
@@ -1,1 +0,0 @@
-FROM amazon/aws-eb-python:3.4.2-onbuild-3.5.1
--- docker/docker.sh
+++ docker/docker.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 rm -fv docker/nando-demo.zip
-rm -fv docker/Dockerfile.local
-cp -v docker/Dockerfile docker/Dockerfile.local
-zip docker/nando-demo.zip docker/Dockerfile.local docker/application.py docker/requirements.txt
+zip docker/nando-demo.zip docker/Dockerfile docker/application.py docker/requirements.txt
 aws s3 cp docker/nando-demo.zip s3://nando-automation-demo
 aws cloudformation create-stack --stack-name nando-demo-docker --template-body file://docker/elasticbeanstalk.json
