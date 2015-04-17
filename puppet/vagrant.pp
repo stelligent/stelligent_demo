b5b31485a2025442f65b4dac80d0010fd3021c6c
nando p <fernando.pando@stelligent.com>
2015-04-17 19:51:31 -0400

    Error: Validation of Exec[reload-jenkins] failed: 'java -jar /usr/lib/jenkins/jenkins-cli.jar -s http://localhost:8080 reload-configuration' is not qualified and no path was specified. Please qualify the command or specify a path. at /etc/puppet/modules/jenkins/manifests/cli.pp:41

--- puppet/vagrant.pp
+++ puppet/vagrant.pp
@@ -1,5 +1,27 @@
 node 'vagrant-centos65.hex7.com' {
+
 	include nginx
+	
+	class { jenkins:
+		lts => true,
+	}
+
+        jenkins::plugin { 'python': }
+	jenkins::plugin { 'credentials': }
+	jenkins::plugin { 'github': }
+	jenkins::plugin { 'ssh-credentials': }
+	jenkins::plugin { 'github-api': }
+	jenkins::plugin { 'scm-api': }
+	jenkins::plugin { 'git-client': }
+	jenkins::plugin { 'git': }
+
+	class { jenkins::security:
+		security_model => 'full_control',
+	}
+
+  	jenkins::user { 'nando':
+    		email    => 'fernando.pando@stelligent.com',
+    		password => 'changeme123',
+  	}
+
 }
-	
-	
