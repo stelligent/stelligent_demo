a07a8cd9aff75ea62214f15bf5a7f8f26db41d90
nando p <fernando.pando@stelligent.com>
2015-04-22 15:38:00 -0400

    vagrant mysql

--- puppet/vagrant-mysql.pp
+++ puppet/vagrant-mysql.pp
@@ -1,20 +1,17 @@
 node /^nando-demo-mysql.*/ {
   		
-	$u = file("/vagrant/mysql/NandoDemoDBUser")
-	$p = file("/vagrant/mysql/NandoDemoDBPass")
-
 	Exec { path => "/bin:/sbin:/usr/bin:/usr/sbin" }
 	
 	class { '::mysql::server':
-  		root_password    => 'mysql-root-password',
+  		#root_password    => 'mysql-root-password',
 		remove_default_accounts => true,
   		override_options => { 'mysqld' => { 'max_connections' => '1024' } },
 	}
 	
 	mysql::db { 'nando-demo':
-  		user     => $u,
-  		password => $p,
-  		host     => '*',
+		user		=> "nando-demo-user",
+		password	=> "nando-demo-pass",
+  		host     => '%',
   		grant    => ['SELECT', 'UPDATE'],
 	}
 }
