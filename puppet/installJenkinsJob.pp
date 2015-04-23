fe7a5a9b78071c5bc561c34a9ebde3ed57faab96
nando p <fernando.pando@stelligent.com>
2015-04-23 10:37:55 -0400

    jenkins jobs

--- puppet/installJenkinsJob.pp
+++ puppet/installJenkinsJob.pp
@@ -1,10 +1,22 @@
 node /.*internal$/ {
    	include jenkins
-	jenkins::job { 'GetInstagramImages':
-  		config => template("/etc/puppet/jobGetInstagramImages.xml.erb"),
+	jenkins::job { 'InstagramImagesGet':
+  		config => template("/etc/puppet/jobInstagramImagesGet.xml.erb"),
 	}
-        jenkins::job { 'PublishCodeDeploy':
-                config => template("/etc/puppet/jobPublishCodeDeploy.xml.erb"),
+	jenkins::job { 'InstagramImagesTest':
+  		config => template("/etc/puppet/jobInstagramImagesTest.xml.erb"),
+	}
+	jenkins::job { 'InstagramImagesSave':
+  		config => template("/etc/puppet/jobInstagramImagesSave.xml.erb"),
+	}
+        jenkins::job { 'DeployStage':
+                config => template("/etc/puppet/jobDeployStage.xml.erb"),
+        }
+        jenkins::job { 'DeployStageTests':
+                config => template("/etc/puppet/jobDeployStageTests.xml.erb"),
+        }
+        jenkins::job { 'DeployProduction':
+                config => template("/etc/puppet/jobDeployProduction.xml.erb"),
         }
 
 }
