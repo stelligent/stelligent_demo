385ef269bb8a5fd0bfc1fae2dfec6e96fd2cc34a
Fernando J Pando <fernando.pando@stelligent.com>
2015-04-24 11:29:41 -0400

    docker

--- docker-centos/application.py
+++ docker-centos/application.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from werkzeug.contrib.fixers import ProxyFix
 app = Flask(__name__)
 
 @app.route('/')
+
 def hello_world():
     return 'nando-automation-demo via docker!!'
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == '__main__':
     app.run()
