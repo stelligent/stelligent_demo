6d77913e27a99e6b833c3f2db5c89f10bddd45b3
Fernando J Pando <fernando.pando@stelligent.com>
2015-05-09 23:41:47 -0400

    moved jobs to dsl

--- puppet/installJenkinsJob.pp
+++ puppet/installJenkinsJob.pp
@@ -9,24 +9,6 @@ node /.*internal$/ {
 	jenkins::job { 'InstagramImageSave':
   		config => template("/etc/puppet/manifests/jobInstagramImageSave.xml.erb"),
 	}
-        jenkins::job { 'CodeDeployStage':
-                config => template("/etc/puppet/manifests/jobCodeDeployStage.xml.erb"),
-        }
-        jenkins::job { 'CodeDeployStageTests':
-                config => template("/etc/puppet/manifests/jobCodeDeployStageTests.xml.erb"),
-        }
-        jenkins::job { 'CodeDeployProduction':
-                config => template("/etc/puppet/manifests/jobCodeDeployProduction.xml.erb"),
-        }
-        jenkins::job { 'DockerStage':
-                config => template("/etc/puppet/manifests/jobDockerStage.xml.erb"),
-        }
-        jenkins::job { 'DockerStageTests':
-                config => template("/etc/puppet/manifests/jobDockerStageTests.xml.erb"),
-        }
-        jenkins::job { 'DockerProduction':
-                config => template("/etc/puppet/manifests/jobDockerProduction.xml.erb"),
-        }
         jenkins::job { 'seed':
                 config => template("/etc/puppet/manifests/seed.xml.erb"),
         }
