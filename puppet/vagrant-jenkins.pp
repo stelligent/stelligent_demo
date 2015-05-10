685be27fba8711234dfcbd814c3bef4d0226b7d2
Fernando J Pando <fernando.pando@stelligent.com>
2015-05-10 00:11:04 -0400

    jenkins templates

--- puppet/vagrant-jenkins.pp
+++ puppet/vagrant-jenkins.pp
@@ -19,22 +19,15 @@ node /^nando-demo-jenkins.*/ {
 
 
 	jenkins::job { 'InstagramImageGet':
-  		config => template("/etc/puppet/manifests/jobInstagramImageGet.xml.erb"),
+  		config => template("/etc/puppet/templates/jobInstagramImageGet.xml.erb"),
 	}
 	jenkins::job { 'InstagramImageTest':
-  		config => template("/etc/puppet/manifests/jobInstagramImageTest.xml.erb"),
+  		config => template("/etc/puppet/templates/jobInstagramImageTest.xml.erb"),
 	}
 	jenkins::job { 'InstagramImageSave':
-  		config => template("/etc/puppet/manifests/jobInstagramImageSave.xml.erb"),
+  		config => template("/etc/puppet/templates/jobInstagramImageSave.xml.erb"),
 	}
-        jenkins::job { 'DeployStage':
-                config => template("/etc/puppet/manifests/jobDeployStage.xml.erb"),
+        jenkins::job { 'seed':
+                config => template("/etc/puppet/templates/seed.xml.erb"),
         }
-        jenkins::job { 'DeployStageTests':
-                config => template("/etc/puppet/manifests/jobDeployStageTests.xml.erb"),
-        }
-        jenkins::job { 'DeployProduction':
-                config => template("/etc/puppet/manifests/jobDeployProduction.xml.erb"),
-        }
-
 }
