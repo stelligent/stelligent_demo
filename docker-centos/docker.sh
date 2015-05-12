8387a1d7c7afac862d9cf053ed41112cd3589cd5
Fernando J Pando <fernando.pando@stelligent.com>
2015-05-12 08:57:09 -0400

    typo

--- docker-centos/docker.sh
+++ docker-centos/docker.sh
@@ -4,6 +4,6 @@ stackName="nando-demo-docker-$(date +%Y%m%d%H%M%S)"
 rm -fv nando-demo.zip
 zip nando-demo.zip Dockerfile application.py requirements.txt
 aws s3 cp nando-demo.zip s3://nando-automation-demo
-aws s3 cp s://cloudformation-stack-name .
-eb init -i NandoDemoDockerApp -r us-east-1 -p docker -k ../nando-demo-20150512005231.pem 
-eb deploy
+aws s3 cp s3://cloudformation-stack-name .
+/usr/local/bin/eb init -i NandoDemoDockerApp -r us-east-1 -p docker -k ../nando-demo-20150512005231.pem 
+/usr/local/bin/eb deploy
