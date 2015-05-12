564e3d7c7555d21a1405fbd81ff0f687a9a87cb1
Fernando J Pando <fernando.pando@stelligent.com>
2015-05-12 10:56:47 -0400

    docker deploy

--- docker-centos/docker.sh
+++ docker-centos/docker.sh
@@ -15,6 +15,6 @@ aws s3 cp nando-demo.zip s3://nando-automation-demo
 echo
 /usr/local/bin/eb init -i NandoDemoDockerApp -r us-east-1 -p docker -k $stackName
 echo
-/usr/local/bin/eb use NandoDemoDockerApp
+/usr/local/bin/eb use NandoDemoDockerEnvironment
 echo
 /usr/local/bin/eb deploy
