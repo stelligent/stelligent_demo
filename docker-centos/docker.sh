830638c0051dbce330f2c8eaa74e56f3de15fb84
Fernando J Pando <fernando.pando@stelligent.com>
2015-05-12 01:30:42 -0400

    eb deploy

--- docker-centos/docker.sh
+++ docker-centos/docker.sh
@@ -5,5 +5,4 @@ rm -fv nando-demo.zip
 zip nando-demo.zip Dockerfile application.py requirements.txt
 aws s3 cp nando-demo.zip s3://nando-automation-demo
 aws s3 cp s://cloudformation-stack-name .
-eb init -i NandoDemoDockerApp -k $(cat cloudformation-stack-name)
 eb deploy
