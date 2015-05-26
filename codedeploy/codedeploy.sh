07ad2f1a776923ea7ed34031259da4027aba7818
Nicholas DeClario <nicholas.declario@stelligent.com>
2015-05-26 19:50:26 -0400

    Moved create app from deploy script to instance

--- codedeploy/codedeploy.sh
+++ codedeploy/codedeploy.sh
@@ -5,6 +5,12 @@ commitID=$(git rev-parse --verify HEAD)
 echo
 echo "using github commit $commitID"
 echo
+
+aws deploy list-applications --region "${REGION}" --application-name nando-demo || \
+{
+    aws deploy create-application --application-name nando-demo --region "${REGION}"
+}
+
 deployID=$(aws deploy create-deployment --output text --region "${REGION}" --application-name nando-demo  --github-location commitId=$commitID,repository=stelligent/nando_automation_demo --deployment-group-name nando-demo)
 echo
 aws deploy get-deployment --region "${REGION}" --deployment-id $deployID  --query "deploymentInfo.status" --output text
