3bb9de0a266cf02d39e6a0a746c798c2dfc15c0d
Nicholas DeClario <nicholas.declario@stelligent.com>
2015-05-26 20:13:39 -0400

    Added service role arn

--- codedeploy/codedeploy.sh
+++ codedeploy/codedeploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 REGION="$(wget -q http://169.254.169.254/latest/meta-data/placement/availability-zone -O - | sed -e 's/[a-zA-Z]$//')"
+ARN="$(aws iam list-roles | grep -i Arn.*CodeDeploy.*$REGION | sed -e 's/.*: "\(.*\)"/\1/')"
 commitID=$(git rev-parse --verify HEAD)
 echo
 echo "using github commit $commitID"
 echo
+
 
 aws deploy get-application --region "${REGION}" --application-name nando-demo || \
 {
@@ -13,7 +15,7 @@ aws deploy get-application --region "${REGION}" --application-name nando-demo ||
 
 aws deploy get-deployment-group --application-name nando-demo --deployment-group-name nando-demo --region "${REGION}" || \
 {
-    aws deploy create-deployment-group --application-name nando-demo --deployment-group-name nando-demo --region "${REGION}"
+    aws deploy create-deployment-group --application-name nando-demo --deployment-group-name nando-demo  --region "${REGION}" --service-role-arn "${ARN}"
 }
 
 deployID=$(aws deploy create-deployment --output text --region "${REGION}" --application-name nando-demo  --github-location commitId=$commitID,repository=stelligent/nando_automation_demo --deployment-group-name nando-demo)
